9bb155e4b8d5319123a041d244baa8cbadd4eb3d
LiliShishkova <129530357+LiliShishkova@users.noreply.github.com>
2024-03-29 23:23:38 +0200

    ImageToText+notworkingPDF

--- Quizify/app/index.tsx
+++ Quizify/app/index.tsx
@@ -13,13 +13,15 @@ export default index;
 */
 
 import { StyleSheet } from 'react-native';
-import { Text, View } from '@/components/Themed';
+import { View } from '@/components/Themed';
 import ImageToText from '@/components/ImageToText';
+import PDFtoText from '@/components/PDFtoText';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <ImageToText/>
+      
     </View>
   );
 }

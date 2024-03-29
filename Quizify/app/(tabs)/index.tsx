d308b39bfc429cb2d6adb5668b898ff8d85fc2a8
Ilinats <107075825+Ilinats@users.noreply.github.com>
2024-03-29 23:27:07 +0200

    quiz

--- Quizify/app/(tabs)/index.tsx
+++ Quizify/app/(tabs)/index.tsx
@@ -1,15 +1,9 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { StyleSheet, Pressable} from 'react-native';
+import { Text, View, } from '@/components/Themed';
+import { Link } from 'expo-router';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
   );
 }
 
@@ -22,10 +16,5 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
